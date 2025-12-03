72cac958e6b0c67586ca9f781a497f75c91178fb
Ryan Mehr <ryanmehr2005.development@gmail.com>
2025-12-02 23:00:28 -0500

    Calling it a day. Will fix this portfolio page sometime when needed.

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -5,7 +5,7 @@ fetch('./static/html/aboutme.html')
         document.getElementById('aboutMe').innerHTML = data;
     });
 
-// Load in technical skills.
+// Load in education.
 fetch('./static/html/education.html')
     .then(response => response.text())
     .then(data => {
@@ -18,3 +18,39 @@ fetch('./static/html/technicalskills.html')
     .then(data => {
         document.getElementById('technicalSkills').innerHTML = data;
     });
+
+// Load in experience.
+fetch('./static/html/experience.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('experience').innerHTML = data;
+    });
+
+// Load in projects.
+fetch('./static/html/projects.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('projects').innerHTML = data;
+    });
+
+// Load in resources.
+fetch('./static/html/resources.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('resources').innerHTML = data;
+    });
+
+// Load in other.
+fetch('./static/html/other.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('other').innerHTML = data;
+    });
+
+// Give in current year.
+const year = new Date().getFullYear();
+const yearElement = document.getElementById("currentYear");
+
+if (yearElement) {
+    yearElement.textContent = year;
+}
